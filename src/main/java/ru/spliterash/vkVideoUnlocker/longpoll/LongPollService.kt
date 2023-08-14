811e2e24d30b01fd2f6ade035cf4d256f2a3c162
Spliterash <me@spliterash.ru>
2023-08-15 00:00:37 +0300

    Обернул весь longpoll в try/catch чтобы не крашилось при ошибках со стороны vk

--- src/main/java/ru/spliterash/vkVideoUnlocker/longpoll/LongPollService.kt
+++ src/main/java/ru/spliterash/vkVideoUnlocker/longpoll/LongPollService.kt
@@ -9,10 +9,10 @@ import jakarta.annotation.PreDestroy
 import jakarta.inject.Singleton
 import kotlinx.coroutines.*
 import okhttp3.Request
-import okhttp3.executeAsync
 import org.apache.commons.logging.LogFactory
 import ru.spliterash.vkVideoUnlocker.common.exceptions.VkUnlockerException
 import ru.spliterash.vkVideoUnlocker.common.okHttp.OkHttpFactory
+import ru.spliterash.vkVideoUnlocker.common.okHttp.executeAsync
 import ru.spliterash.vkVideoUnlocker.longpoll.message.MessageNew
 import ru.spliterash.vkVideoUnlocker.longpoll.message.reply
 import ru.spliterash.vkVideoUnlocker.longpoll.vkModels.LongPollResponse
@@ -39,59 +39,62 @@ class LongPollService(
         val supervisorScope = CoroutineScope(coroutineContext + SupervisorJob())
         log.info("vk longpoll started")
         while (true) {
-            var (key, server, ts) = try {
-                vkApi.groups.getLongPollServer()
+            try {
+                var (key, server, ts) = try {
+                    vkApi.groups.getLongPollServer()
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    delay(10000)
+                    continue
+                }
+
+                while (true) {
+                    val url = "$server?act=a_check&key=$key&ts=$ts&wait=25"
+
+                    val raw = Request.Builder()
+                        .url(url)
+                        .get()
+                        .build()
+                        .executeAsync(client)
+                        .body
+                        .string()
+
+                    val mapped = mapper.readValue<LongPollResponse>(raw)
+
+                    for (update in mapped.updates) {
+                        if (update.type != "message_new")
+                            continue
+
+                        val messageNew = mapper.convertValue<MessageNew>(update.body)
+                        val message = messageNew.message
+
+                        supervisorScope.launch {
+                            try {
+                                messageChainService.proceedMessage(message)
+                            } catch (ex: VkUnlockerException) {
+                                val info = ex.messageForUser()
+                                message.reply(vkApi, info)
+                            } catch (ex: Exception) {
+                                ex.printStackTrace()
+                                message.reply(
+                                    vkApi,
+                                    "Произошла непредвиденная ошибка(${ex.javaClass.simpleName}): ${ex.message}"
+                                )
+                            }
+                        }
+                    }
+
+                    if (mapped.ts == null)
+                        break
+
+                    ts = mapped.ts
+                    ensureActive()
+                }
+                ensureActive()
             } catch (ex: Exception) {
                 ex.printStackTrace()
                 delay(10000)
-                continue
             }
-
-            while (true) {
-                val url = "$server?act=a_check&key=$key&ts=$ts&wait=25"
-
-                val request = Request.Builder()
-                    .url(url)
-                    .get()
-                    .build()
-
-                val raw = client
-                    .newCall(request)
-                    .executeAsync()
-                    .body
-                    .string()
-                val mapped = mapper.readValue<LongPollResponse>(raw)
-
-                for (update in mapped.updates) {
-                    if (update.type != "message_new")
-                        continue
-
-                    val messageNew = mapper.convertValue<MessageNew>(update.body)
-                    val message = messageNew.message
-
-                    supervisorScope.launch {
-                        try {
-                            messageChainService.proceedMessage(message)
-                        } catch (ex: VkUnlockerException) {
-                            val info = ex.messageForUser()
-                            message.reply(vkApi, info)
-                        } catch (ex: Exception) {
-                            ex.printStackTrace()
-                            message.reply(
-                                vkApi,
-                                "Произошла непредвиденная ошибка(${ex.javaClass.simpleName}): ${ex.message}"
-                            )
-                        }
-                    }
-                }
-
-                if (mapped.ts == null)
-                    break
-
-                ts = mapped.ts
-                ensureActive()
-            }
-            ensureActive()
         }
     }
 
